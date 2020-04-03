23f3c6e0cb7e65acb55a80ff76650e008951e244
saarae <saara.malkia@gmail.com>
2020-04-03 11:16:50 +0300

    CRUD

---
+++ src/components/AddTraining.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+export default function AddTraining(props) {
+    const [open, setOpen] = React.useState(false);
+    const [training, setTraining] = React.useState({
+        firstname: '', lastname: '', date: '', activity: '', duration: '', customer: ''
+    })
+
+    const handleClickOpen = () => {
+      setTraining({...training, firstname: props.customer.firstname,
+        lastname: props.customer.lastname, customer: props.customer.links[0].href})
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const handleInputChange = (event) => {
+        setTraining({...training, [event.target.name]: event.target.value})
+    }
+
+    const addTraining = () => {
+        console.log(training);
+        props.saveTraining(training);
+        handleClose();
+    }
+
+    return (
+        <div>
+            <Button size="small" color="primary" onClick={handleClickOpen}>
+                Add training
+            </Button>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add training</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Add a new training session for <b>{training.firstname} {training.lastname}</b>.
+                    </DialogContentText>
+                    <TextField
+                        margin="dense"
+                        name="date"
+                        value={training.date}
+                        label="Date"
+                        onChange={e => handleInputChange(e)}
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        name="activity"
+                        value={training.activity}
+                        label="Activity"
+                        onChange={e => handleInputChange(e)}
+                        fullWidth
+                    />
+                    <TextField
+                        margin="dense"
+                        name="duration"
+                        value={training.duration}
+                        label="Duration (min)"
+                        onChange={e => handleInputChange(e)}
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={addTraining} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,19 +1,43 @@
 import React, { useState, useEffect, forwardRef } from 'react';
 import MaterialTable from "material-table";
-import { Search, ChevronRight, ChevronLeft, FirstPage, LastPage, Clear, ArrowDownward, } from '@material-ui/icons/';
-import AddBoxIcon from '@material-ui/icons/AddBox';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import Button from '@material-ui/core/Button';
+import AddTraining from './AddTraining';
 
 export default function Customerlist() {
     const tableIcons = {
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
         FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
         LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
         ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
         SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-        AddBox: forwardRef((props, ref) => <AddBoxIcon {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />)
-    }
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+      };
 
     const [customers, setCustomers] = useState([]);
     
@@ -23,9 +47,55 @@ export default function Customerlist() {
         fetch('https://customerrest.herokuapp.com/api/customers')
         .then(response => response.json())
         .then(data => setCustomers(data.content))
+        .catch(err => console.error(err))
+    }
+
+    const deleteCustomer = (oldData) => {
+      fetch(oldData.links[0].href, {method: 'DELETE'})
+      // .then(res => fetchData())
+      .catch(err => console.error(err))
+    }
+   
+    const addCustomer = (newData) => {
+      fetch('https://customerrest.herokuapp.com/api/customers', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newData)
+      })
+      .then(res => fetchData())
+      .catch(err => console.error(err))
+    }
+
+    const updateCustomer = (newData, oldData) => {
+      fetch(oldData.links[0].href, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newData)
+      })
+      .then(res => fetchData())
+      .catch(err => console.error(err))
+    }
+
+    const saveTraining = (training) => {
+      console.log(training)
+      fetch('https://customerrest.herokuapp.com/api/trainings', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(training)
+      })
+      .catch(err => console.error(err))
     }
 
     const columns = [
+        {
+            render: rowData => <AddTraining customer={rowData} saveTraining={saveTraining} />
+        },
         {
             title: 'First name',
             field: 'firstname'
@@ -63,14 +133,11 @@ export default function Customerlist() {
                 columns={columns}
                 icons={tableIcons}
                 title="Customers"
-                actions={[
-                    {
-                        icon: 'AddBoxIcon',
-                        tooltip: 'Add a customer',
-                        isFreeAction: true,
-                        onClick: (event) => alert("You want to add a customer")
-                    }
-                ]}
+                editable={{
+                    onRowAdd: newData => addCustomer(newData),          
+                    onRowUpdate: (newData, oldData)  => updateCustomer(newData, oldData),
+                    onRowDelete: oldData => deleteCustomer(oldData)
+                }}
             />
         </div>
     );
